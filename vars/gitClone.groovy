b11b5f4749049b9e3ca321edd927f31c9c146958
MooleMuraliDharaReddy <42165328+MooleMuraliDharaReddy@users.noreply.github.com>
2024-05-27 12:35:43 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,1 +1,9 @@
-
+def call(String branch, String credentialsId, String url) {
+    stage('Clone the repository') {
+        steps {
+            script {
+                git branch: branch, credentialsId: credentialsId, url: url
+            }
+        }
+    }
+}
