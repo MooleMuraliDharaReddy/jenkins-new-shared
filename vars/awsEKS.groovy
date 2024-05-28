641910a73ca6436aa47a024e698d5a0a7fbbe037
MooleMuraliDharaReddy <42165328+MooleMuraliDharaReddy@users.noreply.github.com>
2024-05-28 06:11:37 +0530

    Create awsEKS.groovy

---
+++ vars/awsEKS.groovy
@@ -0,0 +1,6 @@
+def call(credentialsId) {
+  withAWS(credentials: credentialsId, region: 'us-east-1') {
+    // Connect to the EKS cluster
+    sh ' aws eks update-kubeconfig --name dev-cluster --region us-east-1'
+  }
+}
